8d95144dd3d5032e677f1c353f58ec551312c310
wobuzhidaomingzikeyiyouduochangshizhendebuzhidaoyouduochang <zhangxian@daling.com>
2020-03-21 01:17:53 +0800

    add

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+echo 'jinru'
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f https://github.com/Exhuasted/Exhuasted.github.io.git master
+
+
+cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: "前端日志",
+    description: "欢迎访问我的前端日志",
+    head: [
+      ['link', { rel: 'icon', href: '/logo.jpeg' }]
+    ]
+  };
