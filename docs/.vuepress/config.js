1164f7463fa871cdae33cb5b036421c5ccc76c77
wobuzhidaomingzikeyiyouduochangshizhendebuzhidaoyouduochang <zhangxian@daling.com>
2020-04-03 16:29:09 +0800

    add config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,77 +2,72 @@ module.exports = {
     title: "前端知识体系",
     description: "欢迎访问我的前端知识体系",
     head: [
-      ['link', { rel: 'icon', href: '/logo.png' }]
+        ['link', {rel: 'icon', href: '/logo.png'}]
     ],
     themeConfig: {
-      logo: '/logo.png',
-      nav: [],
-      sidebar: {
-          "/blog/":[
-          {
-              title: 'Framework',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: 'DevOps',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: 'JavaScript',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: 'CSS',
-              collapsable: false,
-              sidebarDepth: 2,
-                  children: [
-
-              ]
-          },
-          {
-              title: '编程基础',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: '网络协议',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: '运维相关',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-          {
-              title: '其他',
-              collapsable: false,
-              sidebarDepth: 2,
-              children: [
-
-              ]
-          },
-      ]}
+        repo: "exhuasted/My-blog",
+        logo: '/logo.png',
+        nav: [
+            {
+                text: "博客",
+                link: "/blog/"
+            },
+            {
+                text: "阅读",
+                link: "/book/"
+            }
+        ],
+        sidebar: {
+            "/blog/": [
+                {
+                    title: 'Framework',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: 'DevOps',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: 'JavaScript',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: 'CSS',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: '编程基础',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: '网络协议',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: '运维相关',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+                {
+                    title: '其他',
+                    collapsable: false,
+                    sidebarDepth: 2,
+                    children: []
+                },
+            ]
+        }
     }
-  };
+};
