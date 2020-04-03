4fc53764e40b31de0fcb862d918b243f8ad45ab5
wobuzhidaomingzikeyiyouduochangshizhendebuzhidaoyouduochang <zhangxian@daling.com>
2020-04-03 15:47:51 +0800

    add config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,78 @@
 module.exports = {
-    title: "前端日志",
-    description: "欢迎访问我的前端日志",
+    title: "前端知识体系",
+    description: "欢迎访问我的前端知识体系",
     head: [
-      ['link', { rel: 'icon', href: '/logo.jpeg' }]
-    ]
+      ['link', { rel: 'icon', href: '/logo.png' }]
+    ],
+    themeConfig: {
+      logo: '/logo.png',
+      nav: [],
+      sidebar: {
+          "/blog/":[
+          {
+              title: 'Framework',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: 'DevOps',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: 'JavaScript',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: 'CSS',
+              collapsable: false,
+              sidebarDepth: 2,
+                  children: [
+
+              ]
+          },
+          {
+              title: '编程基础',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: '网络协议',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: '运维相关',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+          {
+              title: '其他',
+              collapsable: false,
+              sidebarDepth: 2,
+              children: [
+
+              ]
+          },
+      ]}
+    }
   };
