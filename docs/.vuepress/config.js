84a79e9b49580c16d2f8be5c1934dc3e68736f2c
zhangxian <zhangxian@ymt360.com>
2021-02-22 20:59:54 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,94 +1,94 @@
 module.exports = {
-    title: "前端知识体系",
-    description: "欢迎访问我的前端知识体系",
-    head: [
-        ['link', {rel: 'icon', href: '/logo.png'}]
+  title: "前端知识体系",
+  description: "欢迎访问我的前端知识体系",
+  head: [
+    ['link', {rel: 'icon', href: '/logo.png'}]
+  ],
+  themeConfig: {
+    repo: "exhuasted/My-blog",
+    logo: '/logo.png',
+    nav: [
+      {
+        text: "博客",
+        link: "/blog/"
+      },
+      {
+        text: "阅读",
+        link: "/book/"
+      }
     ],
-    themeConfig: {
-        repo: "exhuasted/My-blog",
-        logo: '/logo.png',
-        nav: [
-            {
-                text: "博客",
-                link: "/blog/"
-            },
-            {
-                text: "阅读",
-                link: "/book/"
-            }
-        ],
-        sidebar: {
-            "/blog/": [
-                {
-                    title: 'DevOps',
-                    collapsable: false,
-                    children: [
-                        "devops-webpack-step_01",
-                        "devops-webpack-step_02",
-                        "devops-webpack-step_03",
-                        "git"
-                    ]
-                },
-                {
-                    title: 'Framework',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: [
-
-                    ]
-                },
-                {
-                    title: 'JavaScript',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: []
-                },
-                {
-                    title: 'CSS',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: []
-                },
-                {
-                    title: '编程基础',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: []
-                },
-                {
-                    title: '网络协议',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: [
-                        "osi-http"
-                    ]
-                },
-                {
-                    title: '运维相关',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: []
-                },
-                {
-                    title: '其他',
-                    collapsable: false,
-                    sidebarDepth: 2,
-                    children: []
-                },
-            ]
+    sidebar: {
+      "/blog/": [
+        {
+          title: 'DevOps',
+          collapsable: false,
+          children: [
+            "devops-webpack-step_01",
+            "devops-webpack-step_02",
+            "devops-webpack-step_03",
+            "git",
+            "devops-docker",
+            "devops-kubernetes"
+          ]
         },
-        lastUpdated: "更新时间",
-        docsDir: "docs"
+        {
+          title: 'Framework',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+        {
+          title: 'JavaScript',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+        {
+          title: 'CSS',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+        {
+          title: '编程基础',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+        {
+          title: '网络协议',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: [
+            "osi-http"
+          ]
+        },
+        {
+          title: '运维相关',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+        {
+          title: '其他',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: []
+        },
+      ]
     },
-    plugins: [
-        [
-            "@vuepress/pwa",
-            {
-                serviceWorker: true,
-                updatePopup: true,
-            },
-        ],
-        ["@vuepress/medium-zoom", true],
-        ["@vuepress/back-to-top", true],
+    lastUpdated: "更新时间",
+    docsDir: "docs"
+  },
+  plugins: [
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true,
+      },
     ],
+    ["@vuepress/medium-zoom", true],
+    ["@vuepress/back-to-top", true],
+  ],
 };
